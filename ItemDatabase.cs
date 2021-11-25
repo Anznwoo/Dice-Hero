0ba981a9bed3ddd0bfb3d107bc894f38881ce729
ceh1502 <93657218+ceh1502@users.noreply.github.com>
2021-11-26 00:45:14 +0900

    Update ItemDatabase.cs

--- ItemDatabase.cs
+++ ItemDatabase.cs
@@ -12,14 +12,4 @@ public class ItemDatabase : MonoBehaviour
     public List<Item> itemDB = new List<Item>();
                    // public GameObject fieldItemPrefab;
                    // public Vector3[] pos;
-
-    private void Start()
-    {
-       /* for(int i = 0; i < 6; i++)
-        {
-            GameObject go = Instantiate(fieldItemPrefab,pos[i],Quaternion.identity);
-            go.GetComponent<FieldItems>().SetItem(itemDB[Random.Range(0,3)]);
-        }
-    } */
-}
 }
